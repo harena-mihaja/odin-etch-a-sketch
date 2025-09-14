abb3de183bd1931fdd3dc0aee54cb0f94ad0889f
harena-mihaja <mihaja2506@gmail.com>
2025-09-14 07:00:10 +0300

    feat: add some options to enhance UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 const container = document.getElementById("container");
 const divToAdd = document.createDocumentFragment();
-let numberOfTiles = 16; //square per side;
-let containerLength = 900; //px per side
+const colorInput = document.getElementById("color-input");
+const randomizeCheckbox = document.getElementById("randomize-checkbox");
+const showGridCheckbox = document.getElementById("show-grid-checkbox");
 
-const setContainerSize = (numberInPixel) => {
-  containerLength = numberInPixel;
-  return;
-};
+let color = colorInput.value;
+let numberOfTiles = 16; //square per side;
+let containerLength = 600; //px per side
 
 //define container width and height
 container.style.width = `${containerLength}px`;
@@ -18,13 +18,14 @@ const createGridBy = (gridSize) => {
     newDiv.className = "tile";
     newDiv.style.width = `${containerLength / numberOfTiles}px`;
     newDiv.addEventListener("mouseover", () => {
-      newDiv.style.backgroundColor = `rgb(${Math.random() * 255},${
-        Math.random() * 255
-      } ,${Math.random() * 255})`;
+      newDiv.style.backgroundColor = randomizeCheckbox.checked
+        ? `rgb(${Math.random() * 255},${Math.random() * 255} ,${
+            Math.random() * 255
+          })`
+        : `${colorInput.value}`;
     });
     divToAdd.appendChild(newDiv);
   }
-
   container.appendChild(divToAdd);
 };
 
@@ -35,19 +36,36 @@ const clearGrid = () => {
   });
 };
 
+const clearAndCreateGrid = () => {
+  clearGrid();
+  createGridBy(numberOfTiles);
+};
+
 const changeGridSize = () => {
-  numberOfTiles = parseInt(
-    prompt(
-      "Enter the number of square per sides. Must be greater or equal than 16 and less or equal 100."
-    )
-  );
-  while (numberOfTiles < 16 || numberOfTiles > 100) {
-    numberOfTiles = parseInt(
-      prompt("Please enter a number between 16 and 100.")
-    );
+  temp = numberOfTiles;
+  numberOfTiles = parseInt(prompt("Enter grid size (<= 100)"));
+  while (numberOfTiles > 100) {
+    numberOfTiles = parseInt(prompt("Please enter a number below 100"));
+  }
+  if (!numberOfTiles) {
+    numberOfTiles = temp;
+    return;
   }
   clearGrid();
   createGridBy(numberOfTiles);
 };
+
+showGridCheckbox.addEventListener("change", (e) => {
+  const tiles = document.querySelectorAll(".tile");
+  if (e.target.checked) {
+    tiles.forEach((tile) => {
+      tile.style.border = "1px solid";
+    });
+  } else {
+    tiles.forEach((tile) => {
+      tile.style.border = "0px";
+    });
+  }
+});
 
 createGridBy(numberOfTiles);
